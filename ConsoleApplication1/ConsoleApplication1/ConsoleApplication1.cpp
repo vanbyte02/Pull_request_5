c26f68801fcd486c4ed5be1ac503f1189e272fc9
vanbyte02 <liufaivan@gmail.com>
2024-03-12 13:23:36 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int numbers1;
+	cout << "Введите число 1:\n";
+	cin >> numbers1;
+
+	char znak;
+	cout << "Введите знак:\n";
+	cin >> znak;
+
+	int numbers2;
+	cout << "Введите число 2:\n";
+	cin >> numbers2;
+
+	switch (znak) {
+
+	case '+':
+		cout << endl << numbers1 + numbers2;
+		break;
+
+	
+	}
+}
